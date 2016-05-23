9346dc8417209765c332444f8c270f7f11be4306
cbrown78 <chris.brown@vangoghtoyou.com>
2016-05-22 20:57:39 -0500

    Added SMS Class

---
+++ SunLoanDelangIntegration/sms.py
@@ -0,0 +1,57 @@
+import requests
+from xml.etree import ElementTree as ET
+from xml.dom import minidom
+from xml.etree.ElementTree import Element, SubElement, Comment, tostring
+
+
+class SMS(object):
+    def __init__(self):
+        self.baseEndPoint = "http://sunloanapi.dlangemobile.com/xml/messages/"
+        self.contentType = ""
+        self.message = "python test"
+        self.phoneNumber = "+12109191320"
+        self.apiKey = "8dba905330fa4d5a9b5193c4cedb540c"
+        self.headers = {'Content-Type': 'application/xml'}
+        self.messageId = 0
+
+    def Send(self):
+        # http://sunloanapi.dlangemobile.com/xml/messages/8dba905330fa4d5a9b5193c4cedb540c/longcode
+        endpoint = self.baseEndPoint + self.apiKey + "/longcode"
+        root = Element('Message')
+        content = SubElement(root, 'Content')
+        content.text = self.message
+        phonenum = SubElement(root, 'PhoneNumbers')
+        strphonenum = SubElement(phonenum, 'string')
+        strphonenum.text = self.phoneNumber
+
+        response = requests.post(endpoint, data=tostring(root), headers=self.headers)
+
+        status_code = response.status_code
+
+        if status_code == 200:
+            tree = ET.fromstring(response.content)
+            for elem in tree.iter():
+                print
+                elem.tag, elem.attrib, elem.text
+                if elem.tag == "int":
+                    self.messageId = elem.text
+
+
+
+                    # Sample Response from Delange
+                    # <Message>
+                    # 	<Content>Test 2 String content</Content>
+                    # 	<PhoneNumbers><string>+12109191320</string></PhoneNumbers>
+                    # </Message>
+
+                    # sample usage
+                    # mysms = SMS()
+                    # mysms.message = "Hello Chris"
+                    # mysms.Send()
+
+
+
+
+
+
+
