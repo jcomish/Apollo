786fecd9ac4e4888d89afa3a316ad79e47e9574a
cbrown78 <chris.brown@vangoghtoyou.com>
2016-05-25 20:55:11 -0500

    Refactored base.html code and views.py

--- SunLoanDelangIntegration/views.py
+++ SunLoanDelangIntegration/views.py
@@ -13,21 +13,38 @@ def index(request):
     employee = Employee.objects.get(pk=request.user.id)
     store_name = Store.objects.get(pk=employee.store_id)
     customer_list = Customer.objects.filter(store=store_name).order_by('-create_date')[:10]
-    form = CustomerForm(initial={'store': employee.store_id, 'user_id':request.user.id})
+    action = 'add'
+
+     # todo: refactor all the if statements
+    if 'action' in request.GET:
+        action = request.GET['action']
+        if (request.GET['action'] == 'edit') and ('customer_id' in request.GET):
+            # todo: restrict access to storeid from customer object
+            customer = Customer.objects.get(pk=request.GET['customer_id'])
+            form = CustomerForm(instance=customer)
+            action = 'edit'
+        else:
+            form = CustomerForm(initial={'store': employee.store_id, 'user_id':request.user.id})
+    else:
+        form = CustomerForm(initial={'store': employee.store_id, 'user_id': request.user.id})
+
 
     context = {
         'customer_list': customer_list,
         'form': form,
-        'employee': employee,}
-    if request.method == 'POST':
-        customer_form = CustomerForm(request.POST)
-        customer_id = customer_form.save_and_email()
-        return HttpResponseRedirect('/?customer_id='+str(customer_id))
+        'employee': employee,
+        'action' : action,
+    }
 
-    if 'customer_id' in request.GET:
+    if ('customer_id' in request.GET) and (action != 'edit'):
         # todo: restrict access to storeid from customer object
         customer = Customer.objects.get(pk=request.GET['customer_id'])
         context.update({'customer':customer,})
+
+    if request.method == 'POST':
+        customer_form = CustomerForm(request.POST)
+        customer_id = customer_form.save_and_email()
+        return HttpResponseRedirect('/?customer_id=' + str(customer_id))
 
     return render(request, 'base.html', context=context)
 
