466f4e2f0f9668d6def75a24c0d9653ed4c707df
cbrown78 <chris.brown@vangoghtoyou.com>
2016-05-26 19:10:09 -0500

    Added check group logic to DataImport layer for new builds

--- DataImport/services.py
+++ DataImport/services.py
@@ -12,26 +12,29 @@ User = get_user_model()
 def import_users():
     write = ''
     users = [
-        ('test_employee111', 'Test@123!', 'user1_911@example.com', 'employee',),
-        ('test_nonemployee111', 'Test@123!', '1user_011@example.com', 'nonemployee',),
+        ('test_employee', 'Test@123!', 'user1@example.com', 'employee',),
+        ('test_nonemployee', 'Test@123!', 'user2@example.com', 'nonemployee',),
     ]
 
     for username, password, email, group in users:
-        try:
-            user = User.objects.create_user(username=username, email=email)
-            user.set_password(password)
-            user.save()
-            assert authenticate(username=username, password=password)
+        if User.objects.filter(username=username).exists():
+            write = 'end' # todo: will convert to array and store info per user
+        else:
+            try:
+                user = User.objects.create_user(username=username, email=email)
+                user.set_password(password)
+                user.save()
+                assert authenticate(username=username, password=password)
 
-            if Group.objects.filter(name=group).exists():
-                g = Group.objects.get(name=group)
-                g.user_set.add(user)
-            else:
-                g = Group.objects.create(name=group)
-                g.user_set.add(user)
+                if Group.objects.filter(name=group).exists():
+                    g = Group.objects.get(name=group)
+                    g.user_set.add(user)
+                else:
+                    g = Group.objects.create(name=group)
+                    g.user_set.add(user)
 
-            write = "success"
-        except:
-            write = str(format(username, sys.exc_info()[1]))
+                write = "success"
+            except:
+                write = str(format(username, sys.exc_info()[1]))
     return write
 
