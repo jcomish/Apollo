3e2799200b89b6c46aded5fc2529702a3fe870b6
professorx <micahcfreeman@gmail.com>
2016-06-04 11:31:11 -0500

    Added Logging to Welcome Message

--- SunLoan/settings.py
+++ SunLoan/settings.py
@@ -83,16 +83,16 @@ TEMPLATES = [
 WSGI_APPLICATION = 'SunLoan.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/1.9/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'postgres',  # Or path to database file if using sqlite3.
-        'USER': 'cbrown',
+    # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'NotificationsApp',  # Or path to database file if using sqlite3.
+        # The following settings are not used with sqlite3:
+        'USER': 'professorx',
         'PASSWORD': 'password',
-        'HOST': '127.0.0.1',
+        'HOST': 'localhost',
+    # Empty for localhost through domain sockets or           '127.0.0.1' for localhost through TCP.
         'PORT': '',  # Set to empty string for default.
     }
 }
